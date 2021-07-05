20383434855bac7ccddbb9925f6bc650de18886f
Antonio Casado <antonio.casado@ippen-digital.de>
2021-07-05 05:01:01 +0200

    source plus avro generated

---
+++ src/main/java/de/ippen/cdp/infraestructure/entrypoint/request/UserRequest.java
@@ -0,0 +1,22 @@
+package de.ippen.cdp.infraestructure.entrypoint.request;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder(access = AccessLevel.PUBLIC)
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserRequest {
+
+    private String firstName;
+    private String lastName;
+
+    @Override
+    public String toString() {
+        return "UserRequest{" +
+                " firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/de/ippen/cdp/infraestructure/entrypoint/rest/KafkaDemoController.java
@@ -0,0 +1,31 @@
+package de.ippen.cdp.infraestructure.entrypoint.rest;
+
+import de.ippen.cdp.domain.kafka.KafKaProducerService;
+import de.ippen.cdp.infraestructure.entrypoint.request.UserRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Controller
+@RequestMapping("/send")
+public class KafkaDemoController {
+
+    private KafKaProducerService kafKaProducerService;
+
+
+    public KafkaDemoController(KafKaProducerService kafKaProducerService) {
+        this.kafKaProducerService = kafKaProducerService;
+
+    }
+
+    @PostMapping("/avro")
+    @ResponseStatus(code = HttpStatus.OK)
+    public void sendUserSincronoTemplate(@RequestBody UserRequest userRequest) {
+        kafKaProducerService.sendUserTemplate(userRequest);
+    }
+}
+
+
