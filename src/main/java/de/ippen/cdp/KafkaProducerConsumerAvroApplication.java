8bb1906bf9b5b1c25ef8da317c32d4210103265d
Antonio Casado <antonio.casado@ippen-digital.de>
2021-07-05 04:53:53 +0200

    first commit

---
+++ src/main/java/de/ippen/cdp/KafkaProducerConsumerAvroApplication.java
@@ -0,0 +1,13 @@
+package de.ippen.cdp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaProducerConsumerAvroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaProducerConsumerAvroApplication.class, args);
+    }
+
+}
